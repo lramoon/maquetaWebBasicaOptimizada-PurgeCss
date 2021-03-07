34aa18e1fc54312d9d4abd2aa6c5e9abca38860f
lramoon <bin.ramon.sh@gmail.com>
2021-03-07 16:02:51 -0400

    Repo Inicial de Optimizacion con purge css

---
+++ README.md
@@ -0,0 +1,14 @@
+Propuesta web simple Optimizando css
+
+Se usan algunos cdns desde MDB boostrap4
+
+Usando purge.css, Sass con Sass Compiler
+
+Dentro de la carpeta ./dist Esta ubicado todo el compilado del aplicativo
+Simplemente subiendo la carpeta dist al host puedes apreciar el resultado.
+
+La parte "Dev" es lo que esta fuera de la carpeta dist.
+
+Si gustan pueden utilizarlo, todos los aportes son bienvenidos.
+
+Ramon Lopez-> CodeDealerz
---
+++ js/main.js
@@ -0,0 +1,78 @@
+window.sr = ScrollReveal();
+    //Effects navbar
+  sr.reveal('.navbar',{
+    duration: 2500,
+    origin: 'left',
+    delay: 500
+  });
+    //Effects Epa somos branders
+  sr.reveal('.contenido-1',{
+    duration: 1500,
+    origin: 'bottom',
+    distance: '300px',
+    delay: 500
+  });
+  //Effects contactBrand
+  sr.reveal('.contactBrand',{
+    duration: 1500,
+    origin: 'top',
+    distance: '300px',
+    delay: 500
+  });
+
+
+  //Effects OurServices
+  sr.reveal('#OurServices',{
+    duration: 1500,
+    origin: 'top',
+    distance: '300px',
+    delay: 100
+  });
+  //effects service-1
+  sr.reveal('.service-1',{
+    duration: 1500,
+    origin: 'left',
+    distance: '300px',
+    delay: 800
+  });
+  //effects service-2
+  sr.reveal('.service-2',{
+    duration: 1500,
+    origin: 'left',
+    distance: '300px',
+    delay:800
+  });
+  //effects service-3
+  sr.reveal('.service-3',{
+    duration: 1500,
+    origin: 'right',
+    distance: '300px',
+    delay: 800
+  });
+  //effects service-4
+  sr.reveal('.service-4',{
+    duration: 1500,
+    origin: 'right',
+    distance: '300px',
+    delay: 800
+  }); 
+  //effects service-5
+  sr.reveal('.service-5',{
+    duration: 1500,
+    origin: 'left',
+    distance: '300px',
+    delay: 800
+  }); 
+  //effects service-6
+  sr.reveal('.service-6',{
+    duration: 1500,
+    origin: 'left',
+    distance: '300px',
+    delay: 800
+  }); 
+    //Effects footer
+  sr.reveal('#contact',{
+    duration: 1500,
+    origin: 'bottom',
+    delay: 200
+  });
